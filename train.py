e086b308a4dfc25ede2e05e0318f0042cee53ed1
Rishav Raj <143750240+RishavRaj1604@users.noreply.github.com>
2026-04-20 20:09:56 +0530

    Update train.py

--- train.py
+++ train.py
@@ -87,7 +87,7 @@ def train_and_score(network, dataset):
     model.fit(
         x_train, y_train,
         batch_size=batch_size,
-        epochs=5,   # 🔥 HUGE SPEED BOOST (was 10000 💀)
+        epochs=3,   # 🔥 HUGE SPEED BOOST (was 10000 💀)
         verbose=0,
         validation_data=(x_test, y_test),
         callbacks=[early_stopper]
